62d9561a9f427ed25c03ea6b87020452c43a1d77
Brian Kabiro <briankigondu@gmail.com>
2017-03-31 22:03:50 +0300

    working!!

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 module.exports = function(bp) {
   bp.middlewares.load()
+
+	bp.hear({
+		platform:'facebook',
+		type:'message',
+		text:'hello world'
+	}, (event, next) => {
+		const id = event.user.id
+		const last_name = event.user.last_name
+		const first_name = event.user.first_name
+
+		const text = 'Congrats ' + first_name + " " + last_name
+		bp.messenger.sendText(id, text)
+	})
+
+	bp.hear({
+		platform:'facebook',
+		type:'message',
+		text: /.+/i
+	}, (event, next) => {
+		bp.messenger.sendText(event.user.id, "Sorry I only anser to hello world ....")
+	})
 }
